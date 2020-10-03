7b6e23e65aea600d009d9ceaddf942495be56fe6
Paid_co <hellopaidco@gmail.com>
2020-10-03 12:39:14 -0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/App.scss';
+import '../styles/globals.css';
+import '../styles/shards.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
